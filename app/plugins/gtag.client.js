62d0e67fb0490183ba8c541f0e4594b622de2b08
SAMUEL TARIF <samuel.tarif@gmail.com>
2026-03-04 08:31:42 -0300

    2026-03-04 14:50 - Fix: Removido plugin GA duplicado + window.gtag agora funciona corretamente

--- app/plugins/gtag.client.js
+++ app/plugins/gtag.client.js
@@ -1,20 +1,26 @@
 export default defineNuxtPlugin(() => {
   const GA_MEASUREMENT_ID = 'G-S0038L1Q6R'
   
+  // Inicializar dataLayer ANTES de carregar o script
+  window.dataLayer = window.dataLayer || []
+  
+  // Criar função gtag e tornar global
+  function gtag() {
+    window.dataLayer.push(arguments)
+  }
+  
+  // Expor gtag globalmente
+  window.gtag = gtag
+  
   // Carregar script do Google Analytics
   const script = document.createElement('script')
   script.async = true
   script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`
   document.head.appendChild(script)
   
-  // Inicializar gtag e tornar global
-  window.dataLayer = window.dataLayer || []
-  window.gtag = function() {
-    window.dataLayer.push(arguments)
-  }
-  
-  window.gtag('js', new Date())
-  window.gtag('config', GA_MEASUREMENT_ID, {
+  // Configurar GA4
+  gtag('js', new Date())
+  gtag('config', GA_MEASUREMENT_ID, {
     page_path: window.location.pathname,
     send_page_view: true
   })
@@ -22,7 +28,7 @@ export default defineNuxtPlugin(() => {
   // Rastrear mudanças de rota (SPA navigation)
   const router = useRouter()
   router.afterEach((to) => {
-    window.gtag('config', GA_MEASUREMENT_ID, {
+    gtag('config', GA_MEASUREMENT_ID, {
       page_path: to.fullPath,
       page_title: document.title
     })
@@ -30,4 +36,20 @@ export default defineNuxtPlugin(() => {
   
   // Log para debug
   console.log('✅ Google Analytics carregado:', GA_MEASUREMENT_ID)
+  console.log('✅ window.gtag disponível:', typeof window.gtag)
+  
+  // Disponibilizar via provide/inject (opcional)
+  return {
+    provide: {
+      gtag
+    }
+  }
 })
+
+// Declaração de tipos TypeScript
+declare global {
+  interface Window {
+    dataLayer: any[]
+    gtag: (...args: any[]) => void
+  }
+}
