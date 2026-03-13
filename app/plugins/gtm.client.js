10a7aebab1a074330a934fc0bf3a718198a46aa2
SAMUEL TARIF <samuel.tarif@gmail.com>
2026-03-13 10:36:19 -0300

    Configura GTM para usar Cloudflare Worker proxy em /metrics/gtm.js

--- app/plugins/gtm.client.js
+++ app/plugins/gtm.client.js
@@ -9,11 +9,11 @@ export default defineNuxtPlugin(() => {
     script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+'/metrics/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KZTR2DHT');`;
 
     document.head.appendChild(script);
 
-    console.log('✅ Google Tag Manager carregado - GTM-KZTR2DHT');
+    console.log('✅ Google Tag Manager carregado via Cloudflare Worker - GTM-KZTR2DHT');
   }
 })
