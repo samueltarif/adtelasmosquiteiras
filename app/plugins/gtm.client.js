673fda3bfdbc8524c919a4dbaf208cd906b54ebe
SAMUEL TARIF <samuel.tarif@gmail.com>
2026-03-13 10:34:44 -0300

    Corrige URL do GTM para usar googletagmanager.com

--- app/plugins/gtm.client.js
+++ app/plugins/gtm.client.js
@@ -9,7 +9,7 @@ export default defineNuxtPlugin(() => {
     script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'/metrics/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KZTR2DHT');`;
 
     document.head.appendChild(script);
